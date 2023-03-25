36a399e9da1392787eba740346266754df12d7cc
Sergio <seregako@yandex.ru>
2023-03-25 17:14:41 +0300

    1

---
+++ src/main/java/ru/yandex/practicum/filmorate/strorage/FilmDBStorage.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.filmorate.strorage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+import java.util.Set;
+
+public class FilmDBStorage implements FilmStorage {
+
+    @Override
+    public void update(Film film) {
+
+    }
+
+    @Override
+    public void clearStorage() {
+
+    }
+
+    @Override
+    public List<Film> findAll() {
+        return null;
+    }
+
+    @Override
+    public void add(Film film) {
+
+    }
+
+    @Override
+    public Set<Film> findPopular() {
+        return null;
+    }
+
+    @Override
+    public void setId(int id) {
+
+    }
+
+    @Override
+    public boolean exist(int filmId) {
+        return false;
+    }
+}
