184fc0513f8329547d9930bb9d4362e7c13da922
Sergio <seregako@yandex.ru>
2023-04-12 21:33:25 +0300

    Поправлен стиль

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -65,7 +65,6 @@ public class FilmControllerTest {
     @AfterEach
     public void clear() {
         service.clearStorage();
-        //storage.setId(0);
 
     }
 
@@ -171,19 +170,22 @@ public class FilmControllerTest {
 
     @SneakyThrows
     @Test
-    void putToVoidIdFilmTest(){Film inValidFilm = new Film("A1", "a11", LocalDate.of(1987, 3, 4),
+    void putToVoidIdFilmTest() {
+        Film inValidFilm = new Film("A1", "a11", LocalDate.of(1987, 3, 4),
                 90, new Mpa(1), new TreeSet<Genre>());
         inValidFilm.setId(1);
         String putedFilmString1 = mapper.writeValueAsString(inValidFilm);
-        try {mockMvc.perform(put("/films").contentType("application/json").content(putedFilmString1))
+        try {
+            mockMvc.perform(put("/films").contentType("application/json").content(putedFilmString1))
                     .andExpect(mvcResult -> mvcResult.getResolvedException().getClass().equals(IllegalArgumentException.class));
             log.info("films Map: ", controller.getAllFilms());
         } finally {
             return;
-        }}
+        }
+    }
 
     @Test
-    void throwException() {//тестирование исключения без mockMvc
+    void throwException() {
         Film inValidFilm = new Film("A", "a1", LocalDate.of(1987, 3, 4),
                 90, new Mpa(1), new TreeSet<Genre>());
         NoIdException ex = Assertions.assertThrows(NoIdException.class, () -> controller.putFilm(inValidFilm));
