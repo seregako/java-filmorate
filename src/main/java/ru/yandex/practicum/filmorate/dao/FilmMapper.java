eb65aa987920ba235f66c54c3b9c3b1aaf78be2e
Sergio <seregako@yandex.ru>
2023-04-12 21:40:46 +0300

    закомитен FilmMapper

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmMapper.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class FilmMapper implements RowMapper<Film> {
+
+    @Override
+    public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Film film = new Film();
+        film.setId(rs.getInt("id"));
+        film.setName(rs.getString("name"));
+        film.setDescription(rs.getString("description"));
+        film.setMpa(new Mpa(rs.getInt("mpa_id")));
+        film.setDuration(rs.getInt("duration"));
+        film.setReleaseDate((rs.getDate("release_date")).toLocalDate());
+        return film;
+    }
+}
